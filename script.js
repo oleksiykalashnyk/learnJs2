81fb7ea8c3fa1028a18d4e4f99af83fb8465f2cf
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-09 05:27:38 +0100

    finish basic CLASS and EXTENDS

--- script.js
+++ script.js
@@ -1,97 +1,42 @@
 'use strict';
-//Learn "THIS"
+//Learn "CLASS"
 
-//1. re-call function
 
-function showThis(a, b) {
-    //in use strict "undefined" but else this = object "window"
-    console.log(this);
+class Rectangle {
 
-    function sum() {
-        //in use strict "undefined" but else this = object "window"
-
-        console.log(this);
-        return a + b;
+    constructor(height, weight) {
+        //this is "super();"
+        this.height = height;
+        this.weight = weight;
     }
-    console.log(sum());
-}
 
-showThis(16, 2);
-
-//2 this in object
-const obj = {
-    a: 20,
-    b: 15,
-    sum: function () {
-        console.log(this);
+    calcArea() {
+        return this.height * this.weight;
     }
-};
-obj.sum();
-
-//3 If this inside function constructor or class
-function User(name) {
-    this.name = name;
-
-    this.showName = function () {
-        console.log("Hello " + this.name);
-    };
-}
-let olek = new User("Olek");
-olek.showName();
-
-console.clear();
-
-//4. Use this
-
-function sayName(a, b) {
-    console.log(this);
-    console.log(this.name + " " + a + " " + b);
 }
 
-const user = {
-    name: "John"
-};
-//4.1 .call()
-sayName.call(user, "Text", 56);
-
-//4.2 .apply()
-sayName.apply(user, ["WOW", 123]);
-
-//4.3 .bind()
-function count(num) {
-    return this * num;
-}
-const double = count.bind(2);
-console.log(double(3)); //6
-console.log(double(9)); //18
-
-console.clear();
-
-//practice
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", function () {
-    this.style.backgroundColor = "red";
-    console.log(this);
-});
-
-//5. callback funtion this
-//CallBack dont have "this" his use parent "this"
-const xxx = {
-    num: 5,
-    sayNumber: function () {
-        const say = () => {
-            console.log(this.num);
-        };
-        say();
+//new class extends for Rectangle
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, weight, text, bgColor) {
+        //must stay first this say EXTENDS all constroct for parent class
+        super(height, weight);
+        //other property
+        this.text = text;
+        this.bgColor = bgColor;
     }
-};
-xxx.sayNumber();
 
-//6. next gen
-const dbl1 = (a) =>{
-    return a*2;
-};
-const dbl2 = a => a*2;
-console.log(dbl1(4));
-console.log(dbl2(44));
+    showMyProperty(){
+        console.log(`Text: ${this.text}, colorBG: ${this.bgColor}`);
+    }
+}
+
+
+const p1 = new Rectangle(100, 32);
+const p2 = new Rectangle(530, 86);
+
+console.log(p1.calcArea());
+console.log(p2.calcArea());
+
+const p3 = new ColoredRectangleWithText(23,42,"WOW THIS TEXT", "red");
+p3.showMyProperty();
+console.log(p3.calcArea());
