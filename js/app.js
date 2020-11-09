d2cd02c66301d665ebabc847b7875b62e3f08e3c
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-09 06:00:38 +0100

    finish used CLASS in project

--- js/app.js
+++ js/app.js
@@ -118,7 +118,7 @@ window.addEventListener("DOMContentLoaded", () => {
         //if use this function clear timer
         clearInterval(modalTimerId);
         //if use this function remove event for scroll
-        window.removeEventListener("scroll",showModalByScroll);
+        window.removeEventListener("scroll", showModalByScroll);
     }
     //3.1 Only one button
     const
@@ -142,7 +142,7 @@ window.addEventListener("DOMContentLoaded", () => {
     modalTriggerAll.forEach(btn => {
         btn.addEventListener("click", openModal);
         modalCloseBtn.addEventListener("click", closeModal);
-    }); 
+    });
 
 
 
@@ -161,15 +161,99 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    //3.4 SetTimeOut show modal aafter 5 sec
-    const modalTimerId = setTimeout(openModal, 5000);
+    //3.4 SetTimeOut show modal aafter 15 sec
+    const modalTimerId = setTimeout(openModal, 15000);
 
     //3.5 window
-    function showModalByScroll(){
-        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             openModal();
-            window.removeEventListener("scroll",showModalByScroll);
+            window.removeEventListener("scroll", showModalByScroll);
         }
     }
-    window.addEventListener("scroll",showModalByScroll);
+    window.addEventListener("scroll", showModalByScroll);
+
+
+
+
+
+    //Part 4 practice USE CLASSes
+
+    class MenuCard {
+        constructor(src, alt, title, descr, prise, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.prise = prise;
+            
+            this.perent = document.querySelector(parentSelector);
+
+            this.transfer = 27;
+            this.changeToUAH();
+
+            
+        }
+
+        changeToUAH() {
+            this.prise = this.prise * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement("DIV");
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src="${this.src}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">${this.title}"</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.prise}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.perent.append(element);
+        }
+    }
+
+    //old used type (NEED USE!)
+    // const div = new MenuCard();
+    // div.render();
+
+
+    //clear HTML
+    document.querySelector(".menu .container").innerHTML = "";
+
+    //fast but bad use type
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        ".menu .container"
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие        продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, равильное        количество белков за счет тофу и импортных вегетарианских стейков. ',
+        21,
+        ".menu .container"
+
+    ).render();
+    
 });
