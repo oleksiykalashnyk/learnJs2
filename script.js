37f03cc3a65110870d6e13fd2f810f4277bc0631
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-09 03:51:36 +0100

    finish 5.

--- script.js
+++ script.js
@@ -1,56 +1,39 @@
 'use strict';
-//Learn window/screen/document
+//Learn function constructor
 
-console.dir(document);
+function User(name, id) {
+    //add property
+    this.name = name;
+    this.id = id;
+    this.human = true;
 
+    //add method
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+    };
+}
 
-const box = document.querySelector(".box"),
-    btn1 = document.querySelector("#btn1"),
-    btn2 = document.querySelector("#btn2");
-    
+//add prototype property
+User.prototype.country = "Poland";
 
-//1. clientWidth and clientHeight
-const width = box.clientWidth;
-const height = box.clientHeight;
+//add prototype method
+User.prototype.exit = function () {
+    console.log(`User ${this.name} out now`);
+};
 
-console.log(width, height);
+//create new Users
+const ivan = new User("Ivan", 1);
+const alex = new User("Alex", 1);
 
-//2. offsetWidth and offsetHeight
-const widthOffset = box.offsetWidth;
-const heightOffset = box.offsetHeight;
+//show
+console.log(ivan);
+console.log(alex);
 
-console.log(widthOffset, heightOffset);
+ivan.hello();
+alex.hello();
 
-//3. scrollWidth and scrollHeight
-const widthscroll = box.scrollWidth;
-const heighscroll = box.scrollHeight;
+console.log(ivan.name + " and " + alex.name);
 
-console.log(widthscroll, heighscroll);
-
-// Practice 1
-btn1.addEventListener("click", () => {
-    box.style.height = (box.scrollHeight + 20)+ "px";
-});
-
-// Practice 2
-btn2.addEventListener("click", () => {
-    console.log(box.scrollTop);
-});
-
-//4. getBoundingClientRect()
-console.log(box.getBoundingClientRect());
-console.log(box.getBoundingClientRect().top);
-
-//5. Cheack css
-const style = window.getComputedStyle(box);
-console.log(style);
-console.log(style.display);
-
-//6. Scroll top
-console.log(document.documentElement.clientWidth);
-console.log(document.documentElement.scrollTop);
-document.documentElement.scrollTop = 0;
-box.scrollTop = 600;
-
-window.scrollBy(0,400);
-window.scrollTo(0,400);
+//use prototype property and methods
+ivan.exit();
+console.log(alex.country);
